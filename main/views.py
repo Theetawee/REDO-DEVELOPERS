5977570c884f415b7b78f088ae5cb82ce44f5c72
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-03-25 18:01:22 +0300

    save

--- main/views.py
+++ main/views.py
@@ -5,5 +5,9 @@ from .models import Testimony
 # Create your views here.
 def index(request):
     testimonies = Testimony.objects.all()
-    context = {"testimonies": testimonies}
+    context = {
+        "testimonies": testimonies,
+        "title": "Redo Developers Inc. | Pioneering Technology Solutions for Success",
+        "description": "Discover a transformative partnership with Redo Developers Inc. We&#x27;re your trailblazing ally, delivering cutting-edge technology and innovative software solutions to propel businesses and individuals towards success in the dynamic digital landscape.",
+    }
     return render(request, "main/index.html", context)
