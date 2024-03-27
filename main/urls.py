7e38e49a7b4b2cca23d204dfe1ee5724e162424d
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-03-27 09:32:56 +0300

    save

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, about_company, company_profiles
+from .views import index, about_company, company_profiles, profile_detail
 
 
 urlpatterns = [
     path("", index, name="home"),
     path("company/about/", about_company, name="about"),
     path("company/profiles/", company_profiles, name="profiles"),
+    path("company/profiles/<slug:slug>/", profile_detail, name="detail"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Testimony
+from .models import CompanyProfile
 
 
 # Create your views here.
@@ -22,9 +23,17 @@ def about_company(request):
 
 
 def company_profiles(request):
+    profiles = CompanyProfile.objects.all()
     context = {
         "title": "Profiles | Redo Developers Inc.",
         "description": "Discover a transformative partnership with Redo Developers Inc. We're your trailblazing ally, delivering cutting-edge technology and innovative software solutions to propel businesses and individuals towards success in the dynamic digital landscape.",
+        "profiles": profiles,
     }
 
     return render(request, "main/profiles.html", context)
+
+
+def profile_detail(request, slug):
+    profile = get_object_or_404(CompanyProfile, slug=slug)
+    context = {"profile": profile}
+    return render(request, "main/profile_detail.html", context)
