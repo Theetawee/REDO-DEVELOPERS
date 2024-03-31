d09f38ad6621c27e1010b6f494707fbe8ed4f7b0
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-03-31 18:24:19 +0300

    save

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from .models import Testimony, CompanyProfile, CompanyRole, TestimonyMsg
-
+from django import forms
+from base.widget import CheckboxSelectMultipleWidget
 # Register your models here.
 
 
+class CompanyProfileForm(forms.ModelForm):
+    class Meta:
+        model = CompanyProfile
+        fields = "__all__"
+        widgets = {
+            "position": CheckboxSelectMultipleWidget(),
+        }
+
+
+class CompanyProfileAdmin(admin.ModelAdmin):
+    form = CompanyProfileForm
+
+
+admin.site.register(CompanyProfile, CompanyProfileAdmin)
+
+
 admin.site.register(Testimony)
-admin.site.register(CompanyProfile)
 admin.site.register(CompanyRole)
 admin.site.register(TestimonyMsg)
