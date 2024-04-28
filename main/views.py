64da235efc94e1f23c35c8b1a57932b3af46debb
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-04-29 00:12:54 +0300

    save

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.core.mail import send_mail
 from django.contrib import messages
 
+
 # Create your views here.
 def index(request):
     testimonies = Testimony.objects.all()
