168c0ece198c3a49ebb795689fa3502dec358f5a
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-07-01 15:41:10 +0300

    save

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,49 +1,7 @@
 from django.conf import settings
-from .models import IPGeolocation
-import requests
-from .utils import get_client_ip
+from django.utils import timezone
 
 
 def app(request):
-    ip = get_client_ip(request)
-
-    try:
-        ip_geolocation = IPGeolocation.objects.get(ip=ip)
-        data = {
-            "APP_NAME": settings.APP_NAME,
-            "location": ip_geolocation,
-        }
-    except IPGeolocation.DoesNotExist:
-        try:
-            url = f"https://ipapi.co/{ip}/json/"  # API endpoint for IP geolocation
-            response = requests.get(url)
-            if response.status_code == 200:
-                data = response.json()
-                # Save fetched data to the database
-                ip_geolocation = IPGeolocation.objects.create(
-                    ip=ip,
-                    city=data.get("city", ""),
-                    country=data.get("country_name", ""),
-                    country_code=data.get("country", ""),
-                    latitude=data.get("latitude", 0),
-                    longitude=data.get("longitude", 0),
-                    timezone=data.get("timezone", ""),
-                )
-                ip_geolocation.save()
-                data = {
-                    "APP_NAME": settings.APP_NAME,
-                    "location": ip_geolocation,
-                }
-            else:
-                data = {
-                    "APP_NAME": settings.APP_NAME,
-                    "location": ip,
-                }
-        except requests.RequestException as e:
-            print(f"Error fetching IP data: {e}")
-            data = {
-                "APP_NAME": settings.APP_NAME,
-                "location": ip,
-            }
-
+    data = {"APP_NAME": settings.APP_NAME, "year": timezone.now().year}
     return data
