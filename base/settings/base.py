b6ef4756ebe991f7ccf41e719a502a0c561500b7
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-07-08 01:21:53 +0300

    save

--- base/settings/base.py
+++ base/settings/base.py
@@ -132,8 +132,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "redodevs@gmail.com"
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("SMTP_EMAIL")
+EMAIL_HOST_PASSWORD = os.environ.get("SMTP_PASSWORD")
 EMAIL_USE_TLS = True
 
 MESSAGE_TAGS = {
